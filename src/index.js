23e4dc8bc67356225f5a6ff67d3dc4b42f2ec84f
Andrea2 <andreab88@hotmail.it>
2025-10-16 17:56:54 +0200

    andato avanti con dataFetcher
    dataFetcher deve capire se manca il numero in una riga, in quel caso,
    mette "1"

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { scryfallRequest } from "./modules/scryfallRequest";
 import { listParser } from "./modules/dataFetcher";
 
 const cards = ["black lotus", "opt", "dispel", "bfdd", "refurbished familiar"];
-const text = "4 black lotus \n 2 opt \n 3 dispel \n\n 5 asd";
+const text = "4 black lotus \n opt \n 3 dispel \n\n 5 asd";
 
 scryfallRequest(cards);
 console.log(listParser(text));
--- src/modules/dataFetcher.js
+++ src/modules/dataFetcher.js
@@ -1,7 +1,10 @@
 function listParser(text) {
     text = text.split("\n").filter(line => line.trim() !== "").map(line => line.trim());
+    const quantity = text.map(line => parseInt(line.split(" ")[0]));
+    const cardName = text.map(line => line.split(" ").slice(1).join(" "));
+    const data = quantity.map((line, index) => ({ quantity: quantity[index], cardName: cardName[index] }));
 
-    return text
+    return data
 }
 
 export { listParser }
