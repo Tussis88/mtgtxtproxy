3d0bd563a9d72e964d2284841c4a4fb649b20c16
Andrea2 <andreab88@hotmail.it>
2025-10-27 17:53:00 +0100

    fixato dataFetcher
    ora datafetcher cancella automaticamente righe con quantità sbagliate.
    
    da fare:
    	- scryfallRequest deve prendere l'array di datafetcher ed
    	  estrarre automaticamente la lista delle carte. oppure deve
    	  essere datafetcher a farlo?
    	- scryfallRequest deve dare un alert se si inseriscono nomi di
    	  carte errate
    	- dataFetcher deve dare un alert se si inseriscono quantità di
    	  carte errate

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import "./styles/styles.css";
 
 import { scryfallRequest } from "./modules/scryfallRequest";
-import { listParser } from "./modules/dataFetcher";
+import { dataFetcher } from "./modules/dataFetcher";
 
 const cards = ["black lotus", "opt", "dispel", "bfdd", "refurbished familiar"];
 const text = "4 black lotus \n opt \n 3 dispel \n\n 5 asd";
 
 scryfallRequest(cards);
-console.log(listParser(text));
+console.log(dataFetcher(text));
--- src/modules/dataFetcher.js
+++ src/modules/dataFetcher.js
@@ -1,10 +1,12 @@
-function listParser(text) {
+function dataFetcher(text) {
     text = text.split("\n").filter(line => line.trim() !== "").map(line => line.trim());
     const quantity = text.map(line => parseInt(line.split(" ")[0]));
     const cardName = text.map(line => line.split(" ").slice(1).join(" "));
     const data = quantity.map((line, index) => ({ quantity: quantity[index], cardName: cardName[index] }));
 
-    return data
+    const filteredData = data.filter(line => !isNaN(line.quantity))
+
+    return filteredData
 }
 
-export { listParser }
+export { dataFetcher }
