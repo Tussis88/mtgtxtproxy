21c4d4a7acec1f96a0d3215c7d7d838b393d09bc
Andrea2 <andreab88@hotmail.it>
2025-11-04 17:59:27 +0100

    proxyGenerator non funziona perchè non è async

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles/styles.css";
 
 import { scryfallRequest } from "./modules/scryfallRequest";
 import { dataFetcher } from "./modules/dataFetcher";
+import { proxyGenerator } from "./modules/proxyGenerator";
 
 const textWrong = "4 black lotus\n opt\n3 dispel\n\n5 asd\n3 fulmine";
 const textOk = "4 black lotus\nopt\n3 dispel\n\n5 lightning bolt";
@@ -11,3 +12,5 @@ console.log(test);
 
 const result = scryfallRequest(test);
 console.log(result);
+
+proxyGenerator(result, test);
--- src/modules/proxyGenerator.js
+++ src/modules/proxyGenerator.js
@@ -2,7 +2,15 @@ function proxyGenerator(cardsArray, userData) {
     const quantityArray = userData.map(line => line.quantity);
     const contentDiv = document.querySelector(".content");
 
-    cardsArray.foreach(cardObj, index => {
+    cardsArray.forEach((cardObj, index) => {
+        for (let i = 0; i < quantityArray[index]; i++) {
+            const cardDiv = document.createElement("div");
+            cardDiv.classList.add("card");
+            cardDiv.textContent = cardObj.name;
 
+            contentDiv.appendChild(cardDiv);
+        }
     })
 }
+
+export { proxyGenerator }
