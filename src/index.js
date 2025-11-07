83335ce581bdc370b41fd0d723394c31dca033c8
Andrea2 <andreab88@hotmail.it>
2025-11-07 17:51:56 +0100

    fatta funzione proxyGenerator

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import { dataFetcher } from "./modules/dataFetcher";
 import { proxyGenerator } from "./modules/proxyGenerator";
 
 const textWrong = "4 black lotus\n opt\n3 dispel\n\n5 asd\n3 fulmine";
-const textOk = "4 black lotus\nopt\n3 dispel\n\n5 lightning bolt";
+const textOk = "4 black lotus\nopt\n3 Writhing Chrysalis\n\n5 lightning bolt\n2 refurbished familiar";
 
 const test = dataFetcher(textOk);
 console.log(test);
 
-const result = scryfallRequest(test);
-console.log(result);
+scryfallRequest(test).then((result) => {
+    console.log(result);
+    proxyGenerator(result, test)
+});
+// const result = scryfallRequest(test);
+// console.log(result);
 
-proxyGenerator(result, test);
+// proxyGenerator(result, test);
--- src/modules/proxyGenerator.js
+++ src/modules/proxyGenerator.js
@@ -1,16 +1,68 @@
 function proxyGenerator(cardsArray, userData) {
     const quantityArray = userData.map(line => line.quantity);
-    const contentDiv = document.querySelector(".content");
 
     cardsArray.forEach((cardObj, index) => {
         for (let i = 0; i < quantityArray[index]; i++) {
-            const cardDiv = document.createElement("div");
-            cardDiv.classList.add("card");
-            cardDiv.textContent = cardObj.name;
-
-            contentDiv.appendChild(cardDiv);
+            createProxyCard(cardObj);
         }
     })
+}
+
+function createProxyCard(cardData) {
+    const contentDiv = document.querySelector(".content");
+
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+    contentDiv.appendChild(cardDiv);
+
+    //TOP: NOME e COSTO DI MANA
+    const topDiv = document.createElement("div");
+    topDiv.classList.add("top");
+    cardDiv.appendChild(topDiv);
+
+    const nameDiv = document.createElement("div");
+    nameDiv.classList.add("name");
+    nameDiv.textContent = cardData.name;
+
+    const manaCostDiv = document.createElement("div");
+    manaCostDiv.classList.add("manaCost");
+    manaCostDiv.textContent = cardData.mana_cost;
+
+    topDiv.appendChild(nameDiv);
+    topDiv.appendChild(manaCostDiv);
+
+    //MID: TIPO e SOTTOTIPO
+    const typeDiv = document.createElement("div");
+    typeDiv.classList.add("type");
+    typeDiv.textContent = cardData.type_line;
+    cardDiv.appendChild(typeDiv);
+
+    //TEXTBOX: TESTO CARTA
+    const oracleDiv = document.createElement("div");
+    oracleDiv.classList.add("oracle");
+    oracleDiv.textContent = cardData.oracle_text;
+    cardDiv.appendChild(oracleDiv);
+
+    //BOT: COLORE e FOR/COS
+    const bottomDiv = document.createElement("div");
+    bottomDiv.classList.add("bottom");
+    cardDiv.appendChild(bottomDiv);
+
+    if (cardData.colors.length >= 1) {
+        const colorDiv = document.createElement("div");
+        colorDiv.classList.add("color");
+        colorDiv.textContent = "color: " + cardData.colors.join(", ");
+
+        bottomDiv.appendChild(colorDiv);
+    }
+
+    if (cardData.power) {
+        const powAndTouDiv = document.createElement("div");
+        powAndTouDiv.classList.add("stats");
+        powAndTouDiv.textContent = cardData.power + "/" + cardData.toughness;
+        bottomDiv.appendChild(powAndTouDiv);
+    }
+
 }
 
 export { proxyGenerator }
